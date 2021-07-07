b8a90d31b6087e9c249d5446640927f9574bdd29
Arthur A <aires.arthur.15@gmail.com>
2021-07-07 01:09:09 -0300

    Update clip.py

--- clip.py
+++ clip.py
@@ -1,10 +1,31 @@
 import pyperclip
+import re 
 
 oldClip = pyperclip.paste()
+regexUrl = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 
 while True:
+	
+	try:
 
-	if oldClip != pyperclip.paste() and pyperclip.paste() != "":
-		
-		oldClip = pyperclip.paste()
-		pyperclip.copy(oldClip.replace('\n',' ').replace('\r',' '))
+		if oldClip != pyperclip.paste() and pyperclip.paste() != "":
+			
+			oldClip = pyperclip.paste()
+
+			newClip = oldClip.replace('\n',' ').replace('\r',' ').replace('⫹','').replace('- ','').replace('⫺','-')
+
+			matches = re.finditer(regexUrl, newClip, re.MULTILINE)
+			
+			for a in enumerate(matches):
+				
+				urlParsed = a[1].group() 
+
+			if urlParsed != newClip.replace(' ',''):
+				pyperclip.copy(newClip)
+			else:
+				pyperclip.copy('![image]({})'.format(newClip))
+
+
+	except AttributeError as e:
+		print('Error: {}'.format(e))
+		exit()
