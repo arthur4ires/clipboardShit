22c6bcbb37fe566d7db5ca4f968ae9409aa9168f
arthur4ires <aires.arthur.15@gmail.com>
2021-07-21 00:52:27 -0300

    add dekstop nofity

--- clip.py
+++ clip.py
@@ -1,35 +1,44 @@
 import pyperclip
 import re 
+from plyer import notification
 
-oldClip = pyperclip.paste()
-regexUrl = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*(png|jpg))"
-urlParsed = ''
+def notifyUser(linkClip):
+    
+     notification.notify(
+            title = 'Link Inválido',
+            message = 'O link {} ainda é inválido, espere o imgur finalizar o upload.'.format(linkClip),
+            timeout  = 5
+        )
+     
+def main():
+    
+    oldClip = pyperclip.paste()
+    regexUrl = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*(png|jpg))"
+    urlParsed = ''
+    
+    while True:
+        try:
+            if oldClip != pyperclip.paste() and pyperclip.paste() != "":
+                oldClip = pyperclip.paste()
+                newClip = oldClip.replace('\n',' ').replace('\r',' ').replace('⫹','').replace('- ','').replace('⫺','-')
+                
+                if 'blob:' in newClip:
+                    notifyUser(newClip)
+                    continue
+                
+                matches = re.finditer(regexUrl, newClip, re.MULTILINE)
+                
+                for a in enumerate(matches):
+                    urlParsed = a[1].group() 
+                
+                if urlParsed != newClip.replace(' ',''):
+                    pyperclip.copy(newClip)
+                else:
+                    pyperclip.copy('![image]({})'.format(newClip))
+                    
+        except AttributeError as e:
+            print('Error: {}'.format(e))
+            exit()
 
-while True:
-	
-	try:
-
-		if oldClip != pyperclip.paste() and pyperclip.paste() != "":
-			
-			oldClip = pyperclip.paste()
-
-			newClip = oldClip.replace('\n',' ').replace('\r',' ').replace('⫹','').replace('- ','').replace('⫺','-')
-
-			matches = re.finditer(regexUrl, newClip, re.MULTILINE)
-			
-			for a in enumerate(matches):
-				
-				urlParsed = a[1].group() 
-
-			if urlParsed != newClip.replace(' ',''):
-			
-				pyperclip.copy(newClip)
-			
-			else:
-			
-				pyperclip.copy('![image]({})'.format(newClip))
-
-
-	except AttributeError as e:
-		print('Error: {}'.format(e))
-		exit()
+if __name__ == '__main__':
+    main()
